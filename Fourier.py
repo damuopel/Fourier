74169e98c751d0058a511a840af64e37fff68839
David <davidmunozpellicer@gmail.com>
2020-08-05 17:32:56 +0200

    Added the core of the function

--- Fourier.py
+++ Fourier.py
@@ -2,21 +2,82 @@ from svgpathtools import svg2paths, wsvg
 import numpy as np
 import matplotlib.pyplot as plt
 
-paths, attributes = svg2paths('Import.svg')
+paths, attributes = svg2paths('Fourier.svg')
 
 Path = paths[0]
 
-plt.ion()
+n = 200
+auxN = np.arange(1,np.ceil(n/2.0)+1)
+N = np.append(0,np.array([auxN,-auxN]).T.flatten())
 
-for i in np.arange(0,1,0.01):
+cReal = np.zeros(N.size)
+cImag = np.zeros(N.size)
 
-    point = Path.point(i)
-
-    x = np.real(point)
-    y = np.imag(point)
+incT = 0.001
+for index, iN in enumerate(N):
+    ic = 0
+    for iT in np.arange(0,1,incT):
+        ic += Path.point(iT)*np.exp(-iN*2*np.pi*1j*iT)*incT
+    cReal[index] = np.real(ic)
+    cImag[index] = np.imag(ic)
     
+t = np.arange(0,1,incT)
+x = np.zeros((t.size,N.size))
+y = np.zeros((t.size,N.size))
+xf = np.zeros(t.size)
+yf = np.zeros(t.size)
+for i,iT in enumerate(t):
+    value = 0
+    for j, iN in enumerate(N):
+        value += complex(cReal[j],cImag[j])*np.exp(-iN*2*np.pi*1j*iT)
+        x[i,j] = np.real(value)
+        y[i,j] = -np.imag(value)
+        
+xf = x[:,-1]      
+yf = y[:,-1]  
+           
+plt.ion()   
 
-    plt.plot(x,y,'bo')
-    
+x -= x[0,0]
+y -= y[0,0]
+xf -= xf[0]
+yf -= yf[0]
+
+for i in np.arange(0,t.size,1):
+    plt.clf()
+    plt.xlim((-500,500))
+    plt.ylim((-700,500))
+    plt.axis('off')
+    plt.plot(xf[:i],yf[:i],'b')
+    for j in range(1,N.size): 
+        plt.plot(x[i,j-1:j+1],y[i,j-1:j+1],'r')
     plt.draw()
     plt.pause(1e-6)
+plt.show(block = True)    
+
+        
+        
+        
+        
+        
+        
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
